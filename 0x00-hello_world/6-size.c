6db5daf94e5722007c160a261fbc6e945f2c41a5
Samuel Yawson <yawsonsamuel@email.com>
2023-09-07 14:28:08 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@ int main(void)
 {
 char c;
 int i;
-long int li;
-long long int lli;
+long l;
+long long ll;
 float f;
 printf("Size of Char: %lu byte(s)\n", sizeof(c));
 printf("Size of Int: %lu byte(s)\n", sizeof(i));
-printf("Size of Long int: %lu byte(s)\n", sizeof(li));
-printf("Size of Long Long int: %lu byte(s)\n", sizeof(lli));
-printf("Size of Long float: %lu byte(s)\n", sizeof(f));
+printf("Size of Long: %lu byte(s)\n", sizeof(l));
+printf("Size of Long Long: %lu byte(s)\n", sizeof(ll));
+printf("Size of Float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
