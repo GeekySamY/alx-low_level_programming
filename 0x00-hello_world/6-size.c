0f85b1c5da55406ed77540b45ff2ae88f74405f0
Samuel Yawson <yawsonsamuel@email.com>
2023-09-07 15:06:55 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int i;
 long l;
 long long ll;
 float f;
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu byte(s)\n", sizeof(i));
-printf("Size of a long: %lu byte(s)\n", sizeof(l));
-printf("Size of a long long: %lu byte(s)\n", sizeof(ll));
-printf("Size of a float: %lu byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long: %d byte(s)\n", sizeof(l));
+printf("Size of a long long: %d byte(s)\n", sizeof(ll));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
