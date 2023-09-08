fb38e4e9f195ee04519ed643a6d6a1e9e0c01313
Samuel Yawson <yawsonsamuel@email.com>
2023-09-08 15:42:01 -0700

    combination single

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on successful execution
+ */
+int main(void)
+{
+	int comb;
+
+	for (comb = 0; comb < 10; comb++)
+	{
+		putchar((comb % 10) + '0');
+		if (comb == 9)
+			continue;
+			putchar(',');
+			putchar(' ');
+
+	}
+	return (0);
+}
