5d6bcb63358e66c7517b3a646e6c882ba8bccfc5
Samuel Yawson <yawsonsamuel@email.com>
2023-09-11 14:53:01 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-	putchar('\n');
 
 	return (0);
 }
