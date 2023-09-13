e36177ef736c9c5d3706acac399d27097c92ffff
Samuel Yawson <yawsonsamuel@email.com>
2023-09-12 23:11:40 -0700

    times table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,8 @@
 
 /**
  * times_table - Prints the 9 times table from 0 to 9
- * Description: this function iterates from 0 to 9 and prints each multiplication
+ * Description: function iterates from 0 to 9 and prints
+ * each multiplication
  * result in the format "X x 9 = Y" where X is the current number
  * being multiplied and Y is the result of the multiplication.
  */
@@ -15,9 +16,22 @@ void times_table(void)
 	for (i = 0; i < 10; i++)
 	{
 		_putchar('0');
-		for ( j = 1; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			product = i * j;
+			if (product <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
