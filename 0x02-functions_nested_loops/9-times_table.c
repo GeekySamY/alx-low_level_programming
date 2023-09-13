272652f174bab8586a104b10c8c677a6dab6d7f9
Samuel Yawson <yawsonsamuel@email.com>
2023-09-13 11:37:45 -0700

    9times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,15 +21,16 @@ void times_table(void)
 		_putchar(' ');
 
 		product = i * j;
-			if (product <= 9)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
+		if (product <= 9)
+		{
+			_putchar(' ');
+		}	
+		else
+		{
+			_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
+		
 		}
 		_putchar('\n');
 	}
