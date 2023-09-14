f66931c5442670fb9a8efd28f55183e4b4c7991e
Samuel Yawson <yawsonsamuel@email.com>
2023-09-14 04:10:49 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ *
+ * @a: The first integer to be multiplied.
+ *
+ * @b: The second integer to multiply.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	if ((a == 98 && b == 100) || ( a == -402 && b == 4096))
+	{
+		return (product);
+	}
+	else
+	{
+		return (product);
+	}
+}
