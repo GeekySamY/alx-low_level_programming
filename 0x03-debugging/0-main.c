a44d1f2156e9d894e8def40ede436c3722cddaa9
Samuel Yawson <yawsonsamuel@email.com>
2023-09-13 23:38:37 -0700

    positive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,7 @@
 /**
  * main - tests function that prints if integer is positive or negative
- * @i: the integer to be added
  * Return: 0
  */
-
-void positive_or_negative(int i);
-
 int main(void)
 {
 	int i;
