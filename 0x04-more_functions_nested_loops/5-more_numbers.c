3fab6cb268d9ede73360dcf7016127ec51fa4aa3
Samuel Yawson <yawsonsamuel@email.com>
2023-09-14 09:50:36 -0700

    straight line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 	for (num1 = 0; num1 < 10; num1++)
 	{
 
-		for (num2 = 0; num2 < 15; num2++)
+		for (num2 = num1 + 1; num2 < 15; num2++)
 		{
-			if (num2 > 10)
+			if (num2 >= 10)
 			{
 			_putchar(num2 / 10 + '0');
 			_putchar(num2 % 10 + '0');
