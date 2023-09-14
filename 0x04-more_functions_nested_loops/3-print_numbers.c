2546598a69ecb862929a2a8b1c2534cb06cc56a9
Samuel Yawson <yawsonsamuel@email.com>
2023-09-14 04:33:10 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line.
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
